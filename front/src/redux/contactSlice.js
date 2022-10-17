d704ea4c95c8ac19da4715687212e29571fbf50b
Lazyuno1987 <95475727+Lazyuno1987@users.noreply.github.com>
2022-10-17 16:22:16 +0300

    Update contactSlice.js

--- front/src/redux/contactSlice.js
+++ front/src/redux/contactSlice.js
@@ -25,8 +25,8 @@ export const contactApi = createApi({
       invalidatesTags: ['Contact'],
       }),
        deleteContact: builder.mutation({
-      query: name => ({
-        url: `/contacts/${name}`,
+      query: id => ({
+        url: `/contacts/${id}`,
         method: 'DELETE',
       }),
       invalidatesTags: ['Contact'],
@@ -38,52 +38,3 @@ export const contactApi = createApi({
 
 export const { useGetContactsQuery, useAddContactMutation, useDeleteContactMutation } = contactApi;
 
-// const { data } = useGetContactsQuery();
-// console.log(data)
-// const state = useSelector()
-// console.log(state)
-
-// const initialState = {
-//   items: [
-//     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//   ],
-//   filter: '',
-// };
-
-// const contactSlice = createSlice({
-//   name: 'contacts',
-//   initialState: initialState,
-//   reducers: {
-//     addContact({ items }, action) {
-//       items.push(action.payload);
-//     },
-//     filterContact(state, action) {
-//       state.filter = action.payload;
-//     },
-//     deletedContact(state, action) {
-//       state.items = state.items.filter(
-//         contact => contact.id !== action.payload
-//       );
-//     },
-//   },
-// });
-
-// // const persistConfig = {
-// //   key: 'contacts',
-// //   storage,
-// // };
-
-// // export const contactsReducer = persistReducer(
-// //   persistConfig,
-// //   contactSlice.reducer
-// // );
-
-// export const { addContact } = contactSlice.actions;
-// export const { filterContact } = contactSlice.actions;
-// export const { deletedContact } = contactSlice.actions;
-
-// export const getContacts = ({ contacts }) => contacts.items;
-// export const getFilteredContact = ({ contacts }) => contacts.filter;
